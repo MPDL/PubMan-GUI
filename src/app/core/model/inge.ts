01852656f6750d5dd7583c03358dcabe863eb9ca
wilhelmfrank <frank@mpdl.mpg.de>
2023-03-23 15:21:39 +0100

    pubman rest service

---
+++ src/app/core/model/inge.ts
@@ -0,0 +1,152 @@
+/* eslint-disable no-unused-vars */
+export interface UserRO {
+  objectId: string;
+  name: string;
+}
+
+export interface BasicRO {
+  objectId: string;
+  name: string;
+  creationDate: Date;
+  lastModificationDate: Date;
+  creator: UserRO;
+  modifier: UserRO;
+}
+
+export interface Grant {
+  role: string;
+  grantType: string;
+  objectRef: string;
+  objectStatus: string;
+  objectName: string;
+  parentName: string;
+}
+
+export interface User extends BasicRO {
+  loginname: string;
+  password: string;
+  email: string;
+  affiliation: BasicRO;
+  active: boolean;
+  grantList: Grant[];
+  topLevelOuIds: string[];
+}
+
+export interface Ou extends BasicRO {
+  predecessorAffiliations: BasicRO[];
+  hasChildren: boolean;
+  childAffiliations: BasicRO[];
+  hasPredecessors: boolean;
+  parentAffiliation: BasicRO;
+  publicStatus: string;
+  metadata: OuMetadata;
+}
+
+export interface OuMetadata {
+  alternativeNames: string[];
+  city: string;
+  countryCode: string;
+  coordinates: Coordinates;
+  identifiers: Identifier[];
+  name: string;
+  type: string;
+  descriptions: string[];
+  startDate: string;
+  endDate: string;
+}
+
+export interface Identifier {
+  typeString: string;
+  id: string;
+}
+
+export interface Coordinates {
+  altitude: number;
+  latitude: number;
+  longitude: number;
+}
+
+export interface Ctx extends BasicRO {
+  state: string;
+  description: string;
+  responsibleAffiliations: BasicRO[];
+  allowedGenres: string[];
+  allowedSubjectinterfaceifications: string[];
+  workflow: string;
+  contactEmail: string;
+}
+
+export enum workflow {
+  SIMPLE,
+  STANDARD
+}
+
+export enum subjects {
+  DDC,
+  ISO639_3,
+  JEL,
+  MPICC_PROJECTS,
+  MPINP,
+  MPIPKS,
+  MPIRG,
+  MPIS_GROUPS,
+  MPIS_PROJECTS,
+  MPIWG_PROJECTS
+}
+
+export enum genres {
+  ARTICLE,
+  BLOG_POST,
+  BOOK,
+  BOOK_ITEM,
+  BOOK_REVIEW,
+  CASE_NOTE,
+  CASE_STUDY,
+  COLLECTED_EDITION,
+  COMMENTARY,
+  CONFERENCE_PAPER,
+  CONFERENCE_REPORT,
+  CONTRIBUTION_TO_COLLECTED_EDITION,
+  CONTRIBUTION_TO_COMMENTARY,
+  CONTRIBUTION_TO_ENCYCLOPEDIA,
+  CONTRIBUTION_TO_FESTSCHRIFT,
+  CONTRIBUTION_TO_HANDBOOK,
+  COURSEWARE_LECTURE,
+  DATA_PUBLICATION,
+  EDITORIAL,
+  ENCYCLOPEDIA,
+  FESTSCHRIFT,
+  FILM,
+  HANDBOOK,
+  INTERVIEW,
+  ISSUE,
+  JOURNAL,
+  MANUAL,
+  MANUSCRIPT,
+  MEETING_ABSTRACT,
+  MONOGRAPH,
+  MULTI_VOLUME,
+  NEWSPAPER,
+  NEWSPAPER_ARTICLE,
+  OPINION,
+  OTHER,
+  PAPER,
+  PATENT,
+  POSTER,
+  PREPRINT,
+  PRE_REGISTRATION_PAPER,
+  PROCEEDINGS,
+  REGISTERED_REPORT,
+  REPORT,
+  REVIEW_ARTICLE,
+  SERIES,
+  SOFTWARE,
+  TALK_AT_EVENT,
+  THESIS
+}
+
+export interface SearchResult {
+  numberOfRecords: number;
+  records: any[];
+}
+
