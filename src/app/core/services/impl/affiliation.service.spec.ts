9c4dccdfa351002e9685ace33a776df0928a5f7b
wilhelmfrank <frank@mpdl.mpg.de>
2023-05-31 14:55:09 +0200

    adopt tests for Affiliation service

--- src/app/core/services/impl/affiliation.service.spec.ts
+++ src/app/core/services/impl/affiliation.service.spec.ts
@@ -1,13 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AffiliationService } from './affiliation.service';
+import { PropertiesService } from '../properties.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
+import * as props from '../../../../assets/properties.json';
+
+const mocked_props = props;
+
+export class PropsFactory {
+  public properties: any = mocked_props;
+}
 describe('AffiliationService', () => {
   let service: AffiliationService;
+  let props: PropertiesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        AffiliationService,
+        {
+          provide: PropertiesService,
+          useClass: PropsFactory
+        }
+      ]
+    });
     service = TestBed.inject(AffiliationService);
+    props = TestBed.inject(PropertiesService);
   });
 
   it('should be created', () => {
--- src/app/core/services/inge-crud.service.spec.ts
+++ src/app/core/services/inge-crud.service.spec.ts
@@ -1,13 +1,35 @@
 import { TestBed } from '@angular/core/testing';
 
 import { IngeCrudService } from './inge-crud.service';
+import * as props from '../../../assets/properties.json';
+import { PropertiesService } from './properties.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+const mocked_props = props;
+
+export class PropsFactory {
+  public properties: any = mocked_props;
+}
 
 describe('IngeCrudService', () => {
   let service: IngeCrudService;
+  let props: PropertiesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        IngeCrudService,
+        {
+          provide: PropertiesService,
+          useClass: PropsFactory
+        }
+      ]
+    });
     service = TestBed.inject(IngeCrudService);
+    props = TestBed.inject(PropertiesService);
   });
 
   it('should be created', () => {
