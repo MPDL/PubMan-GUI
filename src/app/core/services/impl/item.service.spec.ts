b3bb6b2834bf0b8ee57d385d1d7cbfbe32162829
helkv <helk@mpdl.mpg.de>
2023-06-05 11:36:28 +0200

    Add ItemService.createItem() Test

--- src/app/core/services/impl/item.service.spec.ts
+++ src/app/core/services/impl/item.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { ItemService } from './item.service';
 import { PropertiesService } from '../properties.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {ItemVersionVO} from "../../model/model";
 
 describe('ItemService', () => {
   let itemService: ItemService;
@@ -26,11 +27,24 @@ describe('ItemService', () => {
     controller = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(itemService).toBeTruthy();
+  it('#createItem should send a correct HTTP-Request',  () => {
+    const item = <ItemVersionVO>{
+      message : 'testMessage'
+    };
+    const token = 'token';
+    const expectedRequestUrl = mockedIngeRestUri + '/items';
+
+    itemService.createItem(item, token).subscribe();
+
+    const request = controller.expectOne(expectedRequestUrl);
+    expect(request.request.method).toEqual('POST');
+    expect(request.request.headers.get('Authorization')).toEqual('token');
+    expect(request.request.headers.get('Content-Type')).toEqual('application/json');
+    expect(request.request.body).toEqual('{"message":"testMessage"}');
+    controller.verify();
   });
 
-  it('getItem request',  () => {
+  it('#getItem should send a correct HTTP-Request',  () => {
     const itemId = '1';
     const expectedRequestUrl = mockedIngeRestUri + '/items/' + itemId;
 
