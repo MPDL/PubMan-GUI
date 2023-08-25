b4a00bd438d0a81b9db0909e9635320542b0be74
mwalter <walter@mpdl.mpg.de>
2023-08-25 13:33:19 +0200

    creators started running as far as implemented

--- src/app/demo/components/edit-item/edit-item.component.ts
+++ src/app/demo/components/edit-item/edit-item.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
 import { map, take } from 'rxjs';
-import { AlternativeTitleType, CreatorType } from 'src/app/core/model/model';
+import { AlternativeTitleType, CreatorRole, CreatorType } from 'src/app/core/model/model';
 import { FormControl, FormGroup, Validators, FormBuilder, FormArray, AbstractControl, Form, UntypedFormArray } from '@angular/forms';
 import { UntypedFormGroup } from '@angular/forms';
 
@@ -13,8 +13,10 @@ import { UntypedFormGroup } from '@angular/forms';
 })
 export class EditItemComponent {
   item: any;
+  creatorRoleEnum: typeof CreatorRole = CreatorRole;
   creatorTypeEnum: typeof CreatorType = CreatorType;
   alternativeTitleTypeEnum: typeof AlternativeTitleType = AlternativeTitleType;
+
 
   metadata: FormGroup = this.fb.group({
     title: ['TitleCreate'],
@@ -22,6 +24,23 @@ export class EditItemComponent {
       this.fb.group({
         altTitleType: this.fb.control('TypeCreate1'),
         altTitleValue: this.fb.control('ValueCreate1'),
+      })
+    ]),
+    creators: this.fb.array([
+      this.fb.group({
+        creatorType: this.fb.control(this.creatorTypeEnum.PERSON),
+        creatorRole: this.fb.control(this.creatorRoleEnum.ARTIST),
+        person: this.fb.group({
+          personFamilyName: this.fb.control('creatorPersonFamilyNameCreate1'),
+          personGivenName: this.fb.control('creatorPersonGivenNameCreate1'),
+          personOrganisation: this.fb.array([
+            this.fb.group({
+              organizationName: this.fb.control('creatorPersonOrganizationNameCreate1'),
+              organizationAddress: this.fb.control('creatorPersonOrganizationAddressCreate1'),
+              organizationIdentifier: this.fb.control('creatorPersonOrganizationIdCreate1'),
+            })
+          ])
+        })
       })
     ])
   });
@@ -38,17 +57,37 @@ export class EditItemComponent {
       }),
       take(1)
     ).subscribe(() => this.initilizeForm());
-     // is this waiting for subscribe?
+    // is this waiting for subscribe?
   }
 
   initilizeForm() {
     // console.log("this.alternativeTitles['controls'][0]: " + this.alternativeTitles['controls'][0].get('altTitleValue')?.value); // TODO remove!
+    console.log("this.item.metadata.creators[0]: ", this.item.metadata.creators[0]); // TODO remove!
     this.metadata.patchValue({
-        title: [this.item.metadata.title],
-        alternativeTitles: [{
+      title: [this.item.metadata.title],
+      alternativeTitles: [
+        {
           altTitleType: [this.item.metadata.alternativeTitles ? this.item.metadata.alternativeTitles[0].type : 'n/a'],
           altTitleValue: [this.item.metadata.alternativeTitles ? this.item.metadata.alternativeTitles[0].value : 'n/a']
-        }]
+        }
+      ],
+      creators: [
+        {
+          creatorType: [this.item.metadata.creators[0].type],
+          creatorRole: [this.item.metadata.creators[0].role],
+          person: {
+            personFamilyName: [this.item.metadata.creators[0].person.familyName],
+            personGivenName: [this.item.metadata.creators[0].person.givenName],
+            personOrganization: [
+              {
+                organizationName: [this.item.metadata.creators[0].person.organizations[0].name],
+                organizationAddress: [this.item.metadata.creators[0].person.organizations[0].address],
+                organizationIdentifier: [this.item.metadata.creators[0].person.organizations[0].id],
+              }
+            ]
+          }
+        }
+      ]
     })
     console.log("this.alternativeTitles['controls'][0]: " + this.alternativeTitles['controls'][0].get('altTitleValue')?.value); // TODO remove!
   }
@@ -68,11 +107,6 @@ export class EditItemComponent {
     return this.metadata.get('alternativeTitles') as FormArray;
   }
 
-  getaltTitlesFormGroup (i: number) : FormGroup{
-    console.log("FormGroup: ", (<FormArray> this.metadata.get('alternativeTitles'))?.['controls'][i] as FormGroup)
-    return (<FormArray> this.metadata.get('alternativeTitles'))?.['controls'][i] as FormGroup;
-  }
-
   addAlternativeTitles() {
     this.alternativeTitles.push(this.fb.group({
       altTitleType: this.fb.control('TEST'),
@@ -84,18 +118,41 @@ export class EditItemComponent {
     });
     */
   }
-
+  
   updateAlternativeTitleType() {
   }
 
-  getEnumValues(myEnum: Object) : string[] {
+  get creators() {
+    //console.log("get alternativeTitles: " + this.metadata.get('alternativeTitles')?.value)
+    return this.metadata.get('creators') as FormArray;
+  }
+
+  addCreators() {
+    this.alternativeTitles.push(this.fb.group({
+      altTitleType: this.fb.control('TEST'),
+      altTitleValue: this.fb.control('Value1'),
+    }));
+    /*
+    this.alternativeTitles['controls'].forEach( element => {
+      console.log("XXX " + element.get('altTitleType')?.value); // TODO remove!
+    });
+    */
+  }
+
+  updateCreatorType() {
+
+  }
+
+  
+
+  getEnumValues(myEnum: Object): string[] {
     /*
     (<string[]> Object.keys(myEnum)).forEach( element => {
       console.log("XXX " + element);
     })
     */
-    return <string[]> Object.keys(myEnum);
+    return <string[]>Object.keys(myEnum);
   }
-  
+
 
 }
