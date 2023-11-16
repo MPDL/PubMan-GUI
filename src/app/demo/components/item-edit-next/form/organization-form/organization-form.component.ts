6332a921a7ea4e97feec45c4cc58294c930f207c
mwalter <walter@mpdl.mpg.de>
2023-11-16 13:06:26 +0100

    ngClass Problem

--- src/app/demo/components/item-edit-next/form/organization-form/organization-form.component.ts
+++ src/app/demo/components/item-edit-next/form/organization-form/organization-form.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject ,Input, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormArray, FormControl, FormGroup,  FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ControlType, FormBuilderService } from '../../services/form-builder.service';
+import { CreatorType, OrganizationVO } from 'src/app/core/model/model';
 
 @Component({
   selector: 'pure-organization-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './organization-form.component.html',
   styleUrls: ['./organization-form.component.scss']
 })
 export class OrganizationFormComponent {
+  @Input() organization_form!: FormGroup;
+  
+  fbs = inject(FormBuilderService);
 
+  ngOnInit() {
+    this.organization_form.get('name')
+  }
+
+  get type() {
+    return this.organization_form.get('type') as FormControl<ControlType<OrganizationVO>>;
+  }
 }
--- src/app/demo/components/item-edit-next/form/person-form/person-form.component.ts
+++ src/app/demo/components/item-edit-next/form/person-form/person-form.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ControlType, FormBuilderService } from '../../services/form-builder.service';
+import { CreatorType, IdentifierVO } from 'src/app/core/model/model';
+import { FormArray, FormControl, FormGroup,  FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IdentifierFormComponent } from '../identifier-form/identifier-form.component';
 
 @Component({
   selector: 'pure-person-form',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, IdentifierFormComponent],
   templateUrl: './person-form.component.html',
   styleUrls: ['./person-form.component.scss']
 })
 export class PersonFormComponent {
+  @Input() person_form!: FormGroup;
+  @Output() notice = new EventEmitter();
 
+  fbs = inject(FormBuilderService);
+
+  get organizations() {
+    return this.person_form.get('person.organizations') as FormArray;
+  }
+
+  get identifier() {
+    return this.person_form.get('person.identifier') as FormGroup<ControlType<IdentifierVO>>;
+  }
+
+  addPersonOU() {
+    this.organizations.push(this.fbs.organization_FG(null));
+  }
+
+  removePersonOU(i: number) {
+    this.organizations.removeAt(i);
+  }
+
+  handleIdentifierNotification(event: any) {
+    console.log(event)
+  }
 }
